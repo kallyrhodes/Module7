d0549e421941ee40ab6a4372c4c39a899f713b43
kallyrhodes <78708086+kallyrhodes@users.noreply.github.com>
2021-02-28 11:51:26 -0500

    Add files via upload

---
+++ Module #7.py
@@ -0,0 +1,10 @@
+import datetime
+current_time=datetime.datetime.now()
+print(current_time)
+
+from datetime import datetime
+from datetime import timedelta
+
+print (datetime.now()+timedelta(days=1))
+print (datetime.now()+timedelta(seconds=60))
+print (datetime.now()+timedelta(days=730))
